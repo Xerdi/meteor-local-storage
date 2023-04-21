6c3c3d734e001ad4ce28e358519f8b02d5577311
Erik Nijenhuis <erik@xerdi.com>
2023-04-21 13:41:53 +0200

    Update versions

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name: 'xerdi:local-storage',
-    version: '0.0.5',
+    version: '0.0.6',
     summary: 'Local storage helpers for meteor',
     git: 'https://github.com/Xerdi/meteor-local-storage.git',
     documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('2.8.0');
+    api.versionsFrom('2.11.0');
     api.use([
         'ecmascript',
         'reactive-var',
